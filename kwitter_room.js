8db1c3c0e16c92667351a80dafd70945130a10dc
sarayukatukam <86565311+sarayukatukam@users.noreply.github.com>
2021-12-30 14:28:11 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDtEfnqQ6xM0B5U2HENWbtvboss-CmNt0k",
+    authDomain: "project-93-94-95-96-f7a8c.firebaseapp.com",
+    databaseURL: "https://project-93-94-95-96-f7a8c-default-rtdb.firebaseio.com",
+    projectId: "project-93-94-95-96-f7a8c",
+    storageBucket: "project-93-94-95-96-f7a8c.appspot.com",
+    messagingSenderId: "585740105540",
+    appId: "1:585740105540:web:924ce99103605947900911"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
